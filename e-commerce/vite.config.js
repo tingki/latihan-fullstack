60627b4c795298245e3f8aac8f505c5057b2604f
tingkii <rizkiulirfan@gmail.com>
2023-10-20 15:53:05 +0700

    tes

--- e-commerce/vite.config.js
+++ e-commerce/vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
+  base: '/latihan-fullstack/'
 })
